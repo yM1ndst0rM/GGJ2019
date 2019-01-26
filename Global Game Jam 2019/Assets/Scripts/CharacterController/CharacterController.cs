e9bb2d240c3c17506360584a9313f5ace937c001
Farry1 <mail@lukasfabry.de>
2019-01-26 16:37:15 +0100

    added rotation to the character controller, also added a mouse controll possibility

--- Global Game Jam 2019/Assets/Scripts/CharacterController/CharacterController.cs
+++ Global Game Jam 2019/Assets/Scripts/CharacterController/CharacterController.cs
@@ -4,28 +4,35 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    public enum InputType { mouse, controller };
+    public InputType inputType;
+
+
     public float speed;
     float distToGround;
     Rigidbody playerRigidbody;
     public bool canMove = true;
     float moveMagnitude;
-    Transform camera;
     Vector3 camForward;
+    bool strafing;
+    Quaternion mouseRotation;
+
+    float moveHorizontal;
+    float moveVertical;
+    float rotateHorizontal;
+    float rotateVertical;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody>();
-        if (Camera.main != null)
-        {
-            camera = Camera.main.transform;
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        strafing = Input.GetButton("Aiming");
+       
     }
 
     private void FixedUpdate()
@@ -34,31 +41,77 @@ public class CharacterController : MonoBehaviour
             Move();
     }
 
+
+
     void Move()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        camForward = Vector3.Scale(camera.forward, new Vector3(1, 0, 1)).normalized;
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+        rotateHorizontal = Input.GetAxis("HorizontalRight");
+        rotateVertical = Input.GetAxis("VerticalRight");
 
+
+        //camForward = Vector3.Scale(camera.forward, new Vector3(1, 0, 1)).normalized;
 
         moveMagnitude = Mathf.Clamp01(new Vector2(moveHorizontal, moveVertical).magnitude);
-        
-        //Player Rotation related to Camera Rotation
-        Vector3 playerDirection = moveVertical * camForward + moveHorizontal * camera.right;
-        if (playerDirection.sqrMagnitude > 0.0f)
-        {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(playerDirection, Vector3.up), 0.1f);
-        } 
 
-        float velocityY = playerRigidbody.velocity.y;
-        if (velocityY > 0.2f)
-            velocityY = 0.2f;
-                         
+       
+
+        if (strafing)
+        {
+            Debug.Log("Strafing");
+
+            switch (inputType)
+            {
+                case InputType.controller:
+                    HandleControllerRotation();
+                    break;
+                case InputType.mouse:
+                    HandleMouseRotation();
+                    break;
+            }
+        } else
+        {
+            Vector3 playerDirection = Vector3.right * moveHorizontal + Vector3.forward * moveVertical;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(playerDirection, Vector3.up), 0.1f);
+        }
+
+
         //Apply Movement related to Camera Rotation
-        Vector3 movement = moveVertical * camForward + moveHorizontal * camera.right;
+        //Vector3 movement = moveVertical * camForward + moveHorizontal * camera.right;
+        Vector3 movement = Vector3.right * moveHorizontal + Vector3.forward * moveVertical;
         Vector3 clampedMovement = Vector3.ClampMagnitude(movement, 1);
         Vector3 moveVelocity = clampedMovement * speed;
 
         playerRigidbody.velocity = moveVelocity;
+    }
+
+    void HandleControllerRotation()
+    {
+        Vector3 playerDirection = Vector3.right * rotateHorizontal + Vector3.forward * rotateVertical;
+        if (playerDirection.sqrMagnitude > 0.0f)
+        {            
+            transform.Rotate(0, rotateVertical, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(playerDirection, Vector3.up), 0.1f);
+        }
+    }
+
+    void HandleMouseRotation()
+    {
+        Vector3 mousePos = new Vector3(0, 0, 0);
+        Vector3 mouseDirection;
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            mousePos = hit.point;
+        }
+
+        mouseDirection = mousePos - transform.position;
+
+        // transform.rotation = Quaternion.Euler(mouseDirection);
+        transform.LookAt((new Vector3(mouseDirection.x, transform.position.y, mouseDirection.z)) + new Vector3(transform.position.x, 0, transform.position.z));
     }
 }
