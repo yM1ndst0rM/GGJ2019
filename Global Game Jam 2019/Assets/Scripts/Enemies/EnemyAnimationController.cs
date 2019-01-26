ebb6ac43031dfd72927331d1a43522d14a588264
Farry1 <mail@lukasfabry.de>
2019-01-26 20:46:38 +0100

    Enemy Prefab and Player Die Particle Effect

---
+++ Global Game Jam 2019/Assets/Scripts/Enemies/EnemyAnimationController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimationController : MonoBehaviour
+{
+
+    [Range(0,1)] public float moveMagnitude;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementAnimation();
+    }
+
+    void MovementAnimation()
+    {
+        anim.SetFloat("Speed", moveMagnitude);
+    }
+
+    public void Attack()
+    {
+        anim.SetTrigger("Attack");
+    }
+
+    
+}
