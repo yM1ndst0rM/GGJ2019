91b4c1e41852c0d84b09f501f813365f4e93ef8f
Farry1 <mail@lukasfabry.de>
2019-01-26 12:02:52 +0100

    FMOD Bank Build and Character Direction related to Camera Rotation

--- Global Game Jam 2019/Assets/Scripts/CharacterController/CharacterController.cs
+++ Global Game Jam 2019/Assets/Scripts/CharacterController/CharacterController.cs
@@ -9,11 +9,17 @@ public class CharacterController : MonoBehaviour
     Rigidbody playerRigidbody;
     public bool canMove = true;
     float moveMagnitude;
+    Transform camera;
+    Vector3 camForward;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody>();
+        if (Camera.main != null)
+        {
+            camera = Camera.main.transform;
+        }
     }
 
     // Update is called once per frame
@@ -31,21 +37,25 @@ public class CharacterController : MonoBehaviour
     void Move()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");       
+        float moveVertical = Input.GetAxis("Vertical");
+        camForward = Vector3.Scale(camera.forward, new Vector3(1, 0, 1)).normalized;
+
 
         moveMagnitude = Mathf.Clamp01(new Vector2(moveHorizontal, moveVertical).magnitude);
-        Vector3 playerDirection = Vector3.right * moveHorizontal + Vector3.forward * moveVertical;
-
+        
+        //Player Rotation related to Camera Rotation
+        Vector3 playerDirection = moveVertical * camForward + moveHorizontal * camera.right;
         if (playerDirection.sqrMagnitude > 0.0f)
         {
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(playerDirection, Vector3.up), 0.1f);
-        }
+        } 
 
         float velocityY = playerRigidbody.velocity.y;
         if (velocityY > 0.2f)
             velocityY = 0.2f;
-
-        Vector3 movement = new Vector3(moveHorizontal, velocityY, moveVertical);
+                         
+        //Apply Movement related to Camera Rotation
+        Vector3 movement = moveVertical * camForward + moveHorizontal * camera.right;
         Vector3 clampedMovement = Vector3.ClampMagnitude(movement, 1);
         Vector3 moveVelocity = clampedMovement * speed;
 
