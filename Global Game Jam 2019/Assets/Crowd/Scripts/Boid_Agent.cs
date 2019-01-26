50061dde5db4e2a25a24b38011e8751583a80c74
Martin Misiak <martin.misiak@th-koeln.de>
2019-01-26 22:10:30 +0100

    Created Boid_Agent, Boid_Manager and a basic Player with "Lightintensity" - Added additional assets from /dev to incorporate the enemy model and animations

---
+++ Global Game Jam 2019/Assets/Crowd/Scripts/Boid_Agent.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid_Agent : MonoBehaviour
+{
+
+    private Boid_Manager manager;
+
+    private float      mass                = 1f;
+    private float      drag                = 1.0f;
+    private Vector3    avoidance           = Vector3.zero;
+    public  float      avoidanceWeight     = 1f;
+    private Vector3    cohesion            = Vector3.zero;
+    public  float      cohesionWeight      = 1f;
+    private Vector3    alignment           = Vector3.zero;
+    public  float      alignmentWeight     = 1f;
+
+    private Vector3    chase               = Vector3.zero;
+    public  float      chaseWeight         = 1f;
+
+    private Vector3    runaway             = Vector3.zero;
+    public  float      runawayWeight       = 1f;
+
+
+    private Vector3    resultingForce      = Vector3.zero;
+    private Vector3    currentVelocity     = Vector3.zero;
+    public float       maximumForce        = 1;
+    public float       runawayForce        = 5;
+    private Vector2Int currentGridIdx      = Vector2Int.zero;
+
+    public Player player;
+    public float dps                       = 0.005f;
+    public float rotationSpeed             = 10.00f;
+    public bool runningAway                = false;
+
+    private EnemyAnimationController eac;
+
+    void Awake()
+    {
+        player = GameObject.FindObjectOfType<Player>();
+        eac    = GetComponent<EnemyAnimationController>();
+    }
+    
+    
+
+    void Start()
+    {
+       Boid_Manager[] managers = GameObject.FindObjectsOfType<Boid_Manager>();
+        if (managers.Length > 1 || managers.Length == 0)
+            Debug.LogError("There HAS to be exactly ONE BoidManager in the scene!");
+        else
+            manager = managers[0];
+
+        currentGridIdx  = manager.sortBoidIntoGrid(this);
+    }
+
+    void constrainForcesUpAxis()
+    {
+        avoidance.y = 0;
+        alignment.y = 0;
+        cohesion.y  = 0;
+        chase.y     = 0;
+        runaway.y   = 0;
+    }
+
+    public Vector2Int getCurrentGridIdx()
+    {
+        return currentGridIdx;
+    }
+
+    void updateBehaviourVectors()
+    {
+        Vector3 aggregatedAvoidance = Vector3.zero;
+        Vector3 aggregatedCohesion  = Vector3.zero;
+        chase                       = Vector3.zero;
+        runaway                     = Vector3.zero;
+
+        Vector3 tempVec;
+        int neighbourCount = 0;
+
+        currentGridIdx = manager.sortBoidIntoGrid(this);
+        int x = currentGridIdx.x;
+        int y = currentGridIdx.y;
+
+        List<Boid_Agent> currentNeighbours = new List<Boid_Agent>();
+        currentNeighbours.AddRange(manager.boid_Grid[x, y]);
+
+        for(int i = -1; i <= 1; i++)
+        {
+            y = currentGridIdx.y + i;
+            x = currentGridIdx.x + Random.Range(-1, 0);
+
+            x = Mathf.Clamp(x, 0, manager.getGridResolution() - 1);
+            y = Mathf.Clamp(y, 0, manager.getGridResolution() - 1);
+            currentNeighbours.AddRange(manager.boid_Grid[x, y]);
+        }
+
+        
+        
+
+
+        foreach (Boid_Agent boid in currentNeighbours)
+        {
+            if (boid != this)
+            {
+                Vector3 otherBoidPos = boid.gameObject.transform.position;
+                Vector3 myPos = transform.position;
+                float distanceToBoid = Vector3.Distance(otherBoidPos, myPos);
+
+                tempVec = (myPos - otherBoidPos).normalized;
+                aggregatedAvoidance += tempVec * Mathf.Pow((1f / distanceToBoid), 1);
+                aggregatedCohesion  -= tempVec;
+                neighbourCount++;
+            }
+        }
+
+
+        avoidance = aggregatedAvoidance;
+        cohesion  = aggregatedCohesion * 1f/Mathf.Max(1f,neighbourCount);
+
+        Vector3 towardPlayer = player.transform.position - transform.position;
+        float distance = Vector3.Magnitude(towardPlayer);
+        if (distance < player.lightRadius)
+        {
+            runningAway = true;
+            runaway    += -towardPlayer * Mathf.Pow((1f / distance), 1);
+        }
+        else
+        {
+            runningAway = false;
+            chase      += towardPlayer;
+        }
+    }
+    
+    void updateResultingForce()
+    {
+        
+        resultingForce = avoidance*avoidanceWeight + cohesion*cohesionWeight + chase*chaseWeight + runaway*runawayWeight;
+        float forceMagnitude = resultingForce.magnitude;
+        if(!runningAway)
+            forceMagnitude = Mathf.Clamp(forceMagnitude, 0, maximumForce);
+        else
+            forceMagnitude = Mathf.Clamp(forceMagnitude, 0, runawayForce); 
+
+        resultingForce.Normalize();
+        resultingForce *= forceMagnitude;
+
+
+        //Drag / Friction
+        Vector3 frictionForce = -1f * drag*currentVelocity.magnitude * currentVelocity.normalized;
+        resultingForce += frictionForce;  
+    }
+
+    void updateAnimation()
+    {
+        eac.moveMagnitude = Mathf.Clamp(currentVelocity.magnitude, 0, 1);
+    }
+
+    void Update()
+    {
+        currentGridIdx = manager.sortBoidIntoGrid(this);
+        updateBehaviourVectors();
+        constrainForcesUpAxis();
+        updateResultingForce();
+        updateVelocity();
+        move();
+        updateAnimation();
+        damagePlayer();
+
+    }
+
+    void damagePlayer()
+    {
+        float dmgRadius = 0.05f;
+
+        float distance   = Vector3.Magnitude(player.transform.position - transform.position);
+        if ((distance - dmgRadius) < player.lightRadius)
+        {
+            float damage = dps * Time.deltaTime;
+            player.lightRadius = Mathf.Max(player.lightRadius - damage, 0f);
+        }
+    }
+
+    void updateVelocity()
+    {
+        currentVelocity += (resultingForce / mass)*Time.deltaTime;
+    }
+
+    void move()
+    {
+        Vector3 translationVector = new Vector3(currentVelocity.x, 0, currentVelocity.z);
+        transform.Translate(currentVelocity * Time.deltaTime, Space.World);
+
+        //Rotate & Animate
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, translationVector, rotationSpeed * Time.deltaTime, 0f);
+        transform.rotation = Quaternion.LookRotation(newDir);
+    }
+
+    public Vector2 getAlignment()
+    {
+        return alignment;
+    }
+}
---
+++ Global Game Jam 2019/Assets/Crowd/Scripts/Boid_Manager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid_Manager : MonoBehaviour
+{
+
+    public int spawnNumber = 10;
+    public GameObject agentPrototype;
+    private const int gridResolution = 128;
+    public List<Boid_Agent>[,] boid_Grid = new List<Boid_Agent>[gridResolution, gridResolution];
+    private Bounds worldBounds;
+    private List<BoxCollider> spawnAreas = new List<BoxCollider>();
+
+
+    Bounds computeWorldBB()
+    {
+        Vector3 minExtents = new Vector3(Mathf.Infinity, Mathf.Infinity, Mathf.Infinity);
+        Vector3 maxExtents = new Vector3(Mathf.NegativeInfinity, Mathf.NegativeInfinity, Mathf.NegativeInfinity);
+
+        Collider[] colliders = GameObject.FindObjectsOfType<Collider>();
+
+        foreach (Collider c in colliders)
+        {
+            if (c.bounds.min.x < minExtents.x)
+                minExtents.x = c.bounds.min.x;
+            if (c.bounds.min.y < minExtents.y)
+                minExtents.y = c.bounds.min.y;
+            if (c.bounds.min.z < minExtents.z)
+                minExtents.z = c.bounds.min.z;
+
+            if (c.bounds.max.x > maxExtents.x)
+                maxExtents.x = c.bounds.max.x;
+            if (c.bounds.max.y > maxExtents.y)
+                maxExtents.y = c.bounds.max.y;
+            if (c.bounds.max.z > maxExtents.z)
+                maxExtents.z = c.bounds.max.z;
+        }
+
+        Bounds result = new Bounds();
+        result.SetMinMax(minExtents, maxExtents);
+
+        Debug.Log("WorldBB is: " + minExtents.ToString() + " , " + maxExtents.ToString());
+
+        return result;
+    }
+
+    private void gatherSpawnAreas()
+    {
+        GameObject[] candidates = GameObject.FindGameObjectsWithTag("Spawn");
+        for(int i = 0; i < candidates.Length; i++)
+        {
+            BoxCollider bc = candidates[i].GetComponent<BoxCollider>();
+            if (bc != null)
+                spawnAreas.Add(bc);
+        }
+       
+    }
+
+    public int getGridResolution()
+    {
+        return gridResolution;
+    }
+
+    void Awake()
+    {
+        worldBounds = computeWorldBB();
+        gatherSpawnAreas();
+
+        for (int i = 0; i < gridResolution; i++)
+            for (int j = 0; j < gridResolution; j++)
+                boid_Grid[i, j] = new List<Boid_Agent>();
+    }
+
+
+    void Start()
+    {
+        spawnBoids();
+    }
+
+    public Vector2Int sortBoidIntoGrid(Boid_Agent b)
+    {
+        Vector3 worldPosition = b.transform.position;
+        float relativePositionX = worldPosition.x / worldBounds.size.x;
+        float relativePositionZ = worldPosition.z / worldBounds.size.z;
+        int gridIdxX = (int)(relativePositionX * gridResolution) + (gridResolution / 2) - 1;
+        int gridIdxY = (int)(relativePositionZ * gridResolution) + (gridResolution / 2) - 1;
+
+        //Remove old reference from grid
+        boid_Grid[b.getCurrentGridIdx().x, b.getCurrentGridIdx().y].Remove(b);
+
+        //Add new reference to grid
+        boid_Grid[gridIdxX, gridIdxY].Add(b);
+
+        return new Vector2Int(gridIdxX, gridIdxY);
+
+        //Debug.Log("Sorted into Grid: (" + gridIdxX + "," + gridIdxY + ")");
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    void spawnBoids()
+    {
+        foreach (BoxCollider bc in spawnAreas)
+        {
+            for (int i = 0; i < spawnNumber; i++)
+            {
+                Vector3 randomPosition = new Vector3(Random.Range(bc.bounds.min.x, bc.bounds.max.x),
+                                                     Random.Range(bc.bounds.min.y, bc.bounds.min.y),
+                                                     Random.Range(bc.bounds.min.z, bc.bounds.max.z));
+
+                Instantiate(agentPrototype, randomPosition, Quaternion.identity);
+            }
+        }
+    }
+}
