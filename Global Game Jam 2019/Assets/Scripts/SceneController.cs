56cdc7ed7dbdd32f52e286f4135cb979b571b5fb
Yuliy <mountan_spirit-github@yahoo.de>
2019-01-27 01:30:31 +0100

    Add combiner scene and implement combine loader script

---
+++ Global Game Jam 2019/Assets/Scripts/SceneController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController: MonoBehaviour
+{
+    public SceneAsset[] MainScenes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadScenesAsync());
+    }
+
+    private IEnumerator LoadScenesAsync()
+    {
+
+        foreach (var s in MainScenes)
+        {
+            
+            var asyncLoad = SceneManager.LoadSceneAsync(s.name, LoadSceneMode.Additive);
+            while (!asyncLoad.isDone)
+            {
+                yield return null;
+            }
+        }
+    }
+}
