91f8a5238139d95a49b8dce8388da1048f40f204
Yuliy <mountan_spirit-github@yahoo.de>
2019-01-27 14:02:56 +0100

    Add goal detector script

---
+++ Global Game Jam 2019/Assets/GoalLocationController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+using Debug = UnityEngine.Debug;
+
+public class GoalLocationController : MonoBehaviour
+{
+    public bool HasReachedGoal = false;
+    public UnityEvent ReachedGoal;
+    public GameObject GoalIndicator;
+
+    public Material GoalSpecialMaterial;
+
+    public void Start()
+    {
+        var meshRenderer = GetComponent<MeshRenderer>();
+        var materialArray = meshRenderer.materials;
+
+        materialArray[1] = GoalSpecialMaterial;
+
+        meshRenderer.materials = materialArray;
+
+        GoalIndicator.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, GoalIndicator.transform.position.z);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && !HasReachedGoal)
+        {
+            HasReachedGoal = true;
+            ReachedGoal.Invoke();
+        }
+    }
+}
